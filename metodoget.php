3c8eb889fec85d42dcea2042fbd1e951e9150589
samuel zapata <samuel.zapata0111@gmail.com>
2020-03-27 20:48:32 -0500

    repasamos lo basico de html

---
+++ metodoget.php
@@ -0,0 +1,7 @@
+<?php
+
+print_r($_GET);
+
+$nombre =$_GET['nombre'];
+echo ("su nombre es :".$nombre);
+?>
---
+++ metodopost.php
@@ -0,0 +1,8 @@
+<?php
+
+print_r($_post);
+
+$nombre =$_post['nombre2'];
+echo ("su nombre es :".$nombre);
+echo ("su edad es :".$edad);
+?>
